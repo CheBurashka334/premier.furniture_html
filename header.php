dd5d65c513420e3dd536b00c2a0961ca169de128
myaff <grabka74@gmail.com>
2015-12-29 12:51:23 +0500

    страница
    не сделано моб меню, мод окна и формы в них, анимация

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Premier Мебель</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!--link rel="shortcut icon" href="favicon.ico" type="image/x-icon"-->
+	<!--link rel="shortcut icon" href="favicon.png" type="image/png"-->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<script src="js/materialize.min.js"></script>
+	<script src="js/jquery.jcarousel.min.js"></script>
+	<script src="js/svg-lib.js"></script>
+	<script src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="template_styles.css" />
+</head>
+<body>
+<div id="svg-placeholder" class="hide"></div>
+<div class="layout">
+<div class="page-aside">
+	<button class="btn btn-icon btn-close-menu"><svg class="icon"><use xlink:href="#close"/></svg></button>
+	<div class="page-aside-inner">
+		<div class="logo-box">
+			<a href="index.php"><svg class="logo"><use xlink:href="#logo"/></svg></a>
+		</div>
+		<div class="mobile-menu">
+			<ul class="menu">
+				<li class="menu-item"><a class="menu-link js-anchor-link" href="#company">Компания</a></li>
+				<li class="menu-item"><a class="menu-link js-anchor-link" href="#projects">Проекты</a></li>
+				<li class="menu-item"><a class="menu-link js-anchor-link" href="#reviews">Отзывы</a></li>
+				<li class="menu-item"><a class="menu-link js-anchor-link" href="#contacts">Контакты</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="page">
+	<div class="header start">
+		<div class="container">
+			<div class="row">
+				<div class="btn-menu-box col s2 m1 hide-on-large-only">
+					<button class="btn-menu btn btn-icon hide-on-large-only"></button>
+				</div>
+				<div class="logo-box col m4 offset-m3 l3 hide-on-small-only">
+					<a href="index.php"><svg class="logo"><use xlink:href="#logo"/></svg></a>
+				</div>
+				<div class="main-menu-box col l6 hide-on-med-and-down">
+					<ul class="menu">
+						<li class="menu-item"><a class="menu-link js-anchor-link" href="#company">Компания</a></li>
+						<li class="menu-item"><a class="menu-link js-anchor-link" href="#projects">Проекты</a></li>
+						<li class="menu-item"><a class="menu-link js-anchor-link" href="#reviews">Отзывы</a></li>
+						<li class="menu-item"><a class="menu-link js-anchor-link" href="#contacts">Контакты</a></li>
+					</ul>
+				</div>
+				<div class="cont-box col s10 m4 l3">
+					<a class="phone-link" href="telto:+73452605129">+7&nbsp;(3452)&nbsp;60&nbsp;51&nbsp;29</a>
+					<button class="btn small js-modal hide-on-med-and-down" data-modal="">Оставить заявку</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="workarea">
---
+++ index.php
@@ -0,0 +1,229 @@
+<?php include('header.php'); ?>
+<div class="start-box">
+	<div class="start-box-content">
+		<div class="start-box-title">Кухни на заказ в Тюмени</div>
+	</div>
+</div>
+<div class="company-box">
+	<div class="box-title">Мы не просто изготавливаем кухни, </br>мы стараемся сделать вашу жизнь проще</div>
+	<div class="company-box-item">
+		<div class="container">
+			<div class="row">
+				<div class="col l10 center no-padding">
+					<div class="company-box-item-icon col s12 m3 l2">
+						<div class="icon-box"><svg class="icon"><use xlink:href="#service"/></svg></div>
+					</div>
+					<div class="company-box-item-content col s12 m9 l8">
+						<div class="title">Предлагаем удобный сервис</div>
+						<div class="text">Бесплатный выезд дизайнера и замерщика в удобное для вас время и место.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="company-box-item">
+		<div class="container">
+			<div class="row">
+				<div class="col l10 center no-padding">
+					<div class="company-box-item-icon col s12 m3 l2">
+						<div class="icon-box"><svg class="icon"><use xlink:href="#consult"/></svg></div>
+					</div>
+					<div class="company-box-item-content col s12 m9 l8">
+						<div class="title">Помогаем и консультируем</div>
+						<div class="text">Подскажем как сделать лучше и на чем можно сэкономить.
+						Расскажем&nbsp;о&nbsp;новых трендах в дизайне и подберём решение именно для вас.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="company-box-item">
+		<div class="container">
+			<div class="row">
+				<div class="col l10 center no-padding">
+					<div class="company-box-item-icon col s12 m3 l2">
+						<div class="icon-box"><svg class="icon"><use xlink:href="#understand"/></svg></div>
+					</div>
+					<div class="company-box-item-content col s12 m9 l8">
+						<div class="title">Понимаем и слышим</div>
+						<div class="text">Для нас очень важно решить вашу проблему, сделать кухню именно такой, которая будет отвечать вашим потребностям и помогать в выполнении ежедневных задач.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="company-box-item">
+		<div class="container">
+			<div class="row">
+				<div class="col l10 center no-padding">
+					<div class="company-box-item-icon col s12 m3 l2">
+						<div class="icon-box"><svg class="icon"><use xlink:href="#garantee"/></svg></div>
+					</div>
+					<div class="company-box-item-content col s12 m9 l8">
+						<div class="title">Гарантируем</div>
+						<div class="text">У нас собственное производство, поэтому мы ручаемся за качество нашей продукции и даём гарантию 5 лет нашим клиентам.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="project-box">
+	<div class="container">
+		<div class="row">
+			<div class="box-title">Последние выполненые проекты</div>
+			<div class="box-text col l8 m10 s12 center center-align">
+				<p>Для нас не существует ограничений — мы реализуем проект любого уровня, которым вы будете с удовольствием пользоваться каждый день.</p>
+			</div>
+			<div class="projects-list col l12 m12 s12 center no-padding">
+				<div class="project-item">
+					<div class="carousel project-carousel">
+						<div class="carousel-inner clearfix">
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-1.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-1.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-1.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-1.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-1.jpg"/></div>
+						</div>
+						<div class="carousel-controlls">
+							<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+							<div class="indicator"><span class="current"></span>/<span class="total"></span></div>
+							<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+						</div>
+					</div>
+					<div class="project-price">Примерная стоимость проекта: 1&nbsp;000&nbsp;000</div>
+					<div class="overlay">
+						<div class="project-info card">
+							<div class="project-info-name">Кухня в скандинавском стиле</div>
+							<div class="color-darkgrey text-light small-text">5 фотографий</div>
+							<button class="btn fullwidth js-project">Посмотреть проект</button>
+						</div>
+					</div>
+				</div>
+				<div class="project-item">
+					<div class="carousel project-carousel">
+						<div class="carousel-inner clearfix">
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-2.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-2.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-2.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-2.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-2.jpg"/></div>
+						</div>
+						<div class="carousel-controlls">
+							<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+							<div class="indicator"><span class="current"></span>/<span class="total"></span></div>
+							<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+						</div>
+					</div>
+					<div class="project-price">Примерная стоимость проекта: 1&nbsp;000&nbsp;000</div>
+					<div class="overlay">
+						<div class="project-info card">
+							<div class="project-info-name">Кухня в стиле эклектика</div>
+							<div class="color-darkgrey text-light small-text">5 фотографий</div>
+							<button class="btn fullwidth js-project">Посмотреть проект</button>
+						</div>
+					</div>
+				</div>
+				<div class="project-item">
+					<div class="carousel project-carousel">
+						<div class="carousel-inner clearfix">
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-3.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-3.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-3.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-3.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-3.jpg"/></div>
+						</div>
+						<div class="carousel-controlls">
+							<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+							<div class="indicator"><span class="current"></span>/<span class="total"></span></div>
+							<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+						</div>
+					</div>
+					<div class="project-price">Примерная стоимость проекта: 1&nbsp;000&nbsp;000</div>
+					<div class="overlay">
+						<div class="project-info card">
+							<div class="project-info-name">Кухня в стиле хай—тек</div>
+							<div class="color-darkgrey text-light small-text">5 фотографий</div>
+							<button class="btn fullwidth js-project">Посмотреть проект</button>
+						</div>
+					</div>
+				</div>
+				<div class="project-item">
+					<div class="carousel project-carousel">
+						<div class="carousel-inner clearfix">
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-4.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-4.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-4.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-4.jpg"/></div>
+							<div class="carousel-item col s12 m12 l12"><img src="images/content/project-4.jpg"/></div>
+						</div>
+						<div class="carousel-controlls">
+							<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+							<div class="indicator"><span class="current"></span>/<span class="total"></span></div>
+							<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+						</div>
+					</div>
+					<div class="project-price">Примерная стоимость проекта: 1&nbsp;000&nbsp;000</div>
+					<div class="overlay">
+						<div class="project-info card">
+							<div class="project-info-name">Кухня в стиле минимализм</div>
+							<div class="color-darkgrey text-light small-text">5 фотографий</div>
+							<button class="btn fullwidth js-project">Посмотреть проект</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="action-box">
+	<div class="container center-align">
+		<div class="row">
+			<div class="box-title">У вас есть вопросы?</br>Не знаете с чего начать?</div>
+			<div class="box-text col s12 m8 l6 center">
+				Обратитесь за помощью к нашим специалистам. Это&nbsp;бесплатно, познавательно и ни к чему не обязывает!
+			</div>
+			<button class="btn center">Получить консультацию</button>
+		</div>
+	</div>
+</div>
+<div class="reviews-box">
+	<div class="container">
+		<div class="box-title">Отзывы наших клиентов</div>
+		<div class="row">
+			<div class="col s12 m8 l12 center">
+				<div class="review-item col s12 m8 l6">
+					<div class="video-box">
+						<iframe src="https://www.youtube.com/embed/M5S9BxadI4Y" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="review-item col s12 m8 l6">
+					<div class="video-box">
+						<iframe src="https://www.youtube.com/embed/6otBJk8BJ4g" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="trends-box">
+	<div class="container center-align">
+		<div class="row">
+			<div class="box-title">Актуальные тренды в </br>дизайне кухни в 2016</div>
+			<button class="btn center">Читать</button>
+		</div>
+	</div>
+</div>
+<div class="contacts-box">
+	<div class="container">
+		<div class="box-title">Премьер мебель</div>
+		<div class="contacts center-align">
+			<p class="address">Тюмень, ул. Феликса Аржанова,39</p>
+			<p><a href="telto:+73452605129">+7&nbsp;(3452)&nbsp;60&nbsp;51&nbsp;29</a></p>
+			<p><a href="telto:+73452605179">+7&nbsp;(3452)&nbsp;60&nbsp;51&nbsp;79</a></p>
+			</br>
+			<button class="btn js-modal" data-modal="">Перезвоните мне</button>
+		</div>
+	</div>
+</div>
+<?php include('footer.php'); ?>
